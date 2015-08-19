17fc43c878beb9103d12316fe203416cf1d0be58
Jason Walkow <jason.walkow1@gmail.com>
2015-08-19 14:01:13 -0700

    added js

---
+++ js/app.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+	//Questions
+	var questions = [
+	{question: "Where and when was Hemingway born?",
+	choices: ["Ketchum, ID in 1905", "Oak Park, IL in 1899", "Havana, Cuba in 1912", "Chicago, IL in 1895"],
+	answer: 1, 
+	qNumber: 1},
+
+	{question: "During WWI, Hemingway signed up to be an Ambulance Driver for which country?",
+	choices: ["Italy", "United States", "France", "Great Britain"],
+	answer: 0, 
+	qNumber: 2},
+
+	{question: "Hemingway once said that 'Courage is ____________________.'",
+	choices: ["'fear evolved'", "'being sober early'", "'grace under pressure'", "'facing a lion'"],
+	answer: 2, 
+	qNumber: 3},	
+
+	{question: "In 1954, Hemingway received the Nobel Prize shortly after winning the Pulitzer Prize for which novel?",
+	choices: ["A Farewell to Arms", "For Whom the Bell Tolls", "The Sun Also Rises", "The Old Man and the Sea"],
+	answer: 3, 
+	qNumber: 4},
+
+	{question: "Hemingway once wrote that this city is a 'moveable feast', in that it stays with you wherever you go in life.",
+	choices: ["Barcelona", "Paris", "New York", "Madrid"],
+	answer: 1, 
+	qNumber: 5},
+	];
+
+	//Global Variables
+	var questionNumber = 0;
+	var correctAnswers = 0;
+	var counter = 0;
+
+	var counter = questions.length
+
+	//Create Question
+	function createQuestion(questions) {
+		for (var i = 0; i < questions.length; i++) {
+			
+		}
+	}
+
+});
+
+
+
+
+
